503df3b337828d30d9490153406c7baec219556a
sunnny4u <sunnycse1995@gmail.com>
2019-08-23 03:40:25 +0600

    Can't remember, did a lot of thing

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Course
+
 
 # Create your views here.
+def courseList(request):
+	Courses = Course.objects.all()
+	return render(request, 'courses/course_list.html', {'output':Courses})
